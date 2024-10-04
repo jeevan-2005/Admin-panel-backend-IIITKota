3a7ab6e26a476786a7e6e88b6ace7038a8ab2398
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-04 12:50:10 +0530

    created app using express...

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+// import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+export const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+// app.use(cookieParser());
+
+app.get("/health-check", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: "Health-Check : server is running fine.",
+  });
+});
+
+app.all("*", (req, res, next) => {
+  const error = new Error(`Route ${req.originalUrl} not found!`);
+  error.status = 404;
+  next(err);
+});
+
+app.use(errorMiddleware);

eabaa5ceaeec459d5077d4f654a5cd9711d6d05c
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-04 21:42:25 +0530

    registration route completed...

---
+++ src/routes/user.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { activateUser, registerUser } from "../controllers/user.controller";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser);
+userRouter.post("/activate-user", activateUser);
+
+export default userRouter;

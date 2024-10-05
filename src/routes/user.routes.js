f3d4a6f91139ccc4e0f0b0fafeb828d00a0ee1c3
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-05 20:47:09 +0530

    created login,refreshToken,logout routes...

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { activateUser, registerUser } from "../controllers/user.controller";
+import {
+  activateUser,
+  getNewAccessToken,
+  registerUser,
+  userLogin,
+  userLogout,
+} from "../controllers/user.controller.js";
+import { authorizeRoles, isAuth } from "../middlewares/auth.middleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/activate-user", activateUser);
+userRouter.post("/login", userLogin);
+userRouter.get("/refresh-token", getNewAccessToken);
+userRouter.get("/logout", isAuth ,userLogout);
 
 export default userRouter;
